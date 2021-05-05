76af7fd271e942ea25facbc7b0391346b1db6ca1
Cheryl Craig <cacraig@ucar.edu>
2021-05-05 14:58:42 -0600

    Fix more unit tests

--- test/unit/sample_files/write_init_files/physics_inputs_protect.F90
+++ test/unit/sample_files/write_init_files/physics_inputs_protect.F90
@@ -138,9 +138,9 @@ CONTAINS
 
                   !Read variable from IC file:
 
-                  if (trim(phys_var_stdnames(name_idx)) == 'sea_level_pressure') then
-                     call read_field(file, 'sea_level_pressure', input_var_names(:,name_idx),     &
-                          timestep, slp)
+                  if (trim(phys_var_stdnames(name_idx)) == 'air_pressure_at_sea_level') then
+                     call read_field(file, 'air_pressure_at_sea_level',                           &
+                          input_var_names(:,name_idx), timestep, slp)
                   end if
 
             end select !special indices
