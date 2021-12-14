c009b4646493cd224fb3a0f7601f08dfbf59a8e6
Steve Goldhaber <goldy@ucar.edu>
2021-12-13 20:30:41 -0700

    Updated NUOPC cap builds Cleanup on atm_comp_nuopc.F90 Update Externals.cfg to cam6_3_037, improve test output

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -6,6 +6,37 @@
 
 # We only test python3 now
 PYTHON=python3
+NUMTESTS=0
+NUMERRORS=0
+
+run_doctest() {
+  # Run the doctests in the input python module ($1)
+  echo "Running doctests in ${1}"
+  if [ `grep -c 'import doctest' ${1}` -ne 0 ]; then
+    ${PYTHON} ${1}
+    res=$?
+  else
+    ${PYTHON} -m doctest ${1}
+    res=$?
+  fi
+  if [ $res -ne 0 ]; then
+    echo "ERROR: doctest for ${1} returned ${res}"
+    NUMERRORS=$(( NUMERRORS + 1 ))
+  fi
+  NUMTESTS=$(( NUMTESTS + 1 ))
+}
+
+run_unittest() {
+  # Run the unit tests in the input python module ($1)
+  echo "Running unit tests in ${1}"
+  ${PYTHON} ${1}
+  res=$?
+  if [ $res -ne 0 ]; then
+    echo "ERROR: unittest for ${1} returned ${res}"
+    NUMERRORS=$(( NUMERRORS + 1 ))
+  fi
+  NUMTESTS=$(( NUMTESTS + 1 ))
+}
 
 # Try to find the correct directory
 scriptdir="$( cd $( dirname ${0} ); pwd -P )"
@@ -21,16 +52,23 @@ if [ ! -d "cime_config" ]; then
 fi
 
 # CAM config doctests:
-${PYTHON} cime_config/cam_config.py
-# CAM config unit tests:
-${PYTHON} test/unit/cam_config_unit_tests.py
+run_doctest cime_config/cam_config.py
 # CAM autogen doctests:
-${PYTHON} cime_config/cam_autogen.py
+run_doctest cime_config/cam_autogen.py
 # CAM build cache doctests:
-${PYTHON} cime_config/cam_build_cache.py
+run_doctest cime_config/cam_build_cache.py
 # Registry generator doctests:
-${PYTHON} -m doctest src/data/generate_registry_data.py
+run_doctest src/data/generate_registry_data.py
+# CAM config unit tests:
+run_unittest test/unit/cam_config_unit_tests.py
 # Registry generator unit tests:
-${PYTHON} test/unit/test_registry.py
+run_unittest test/unit/test_registry.py
 # Physics variable init (phys_init) generator unit tests:
-${PYTHON} test/unit/write_init_unit_tests.py
+run_unittest test/unit/write_init_unit_tests.py
+
+# Report
+if [ ${NUMERRORS} -gt 0 ]; then
+  echo "${NUMERRORS} out of ${NUMTESTS} tests FAILED"
+else
+  echo "All ${NUMTESTS} tests PASSED!"
+fi
