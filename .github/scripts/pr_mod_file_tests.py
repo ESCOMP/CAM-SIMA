c8e8e80506fff95374515966194b2c5311e2a65f
Jesse Nusbaumer <nusbaume@ucar.edu>
2023-08-10 15:08:14 -0600

    Update pr_mod_file script to be more pythonic.

--- .github/scripts/pr_mod_file_tests.py
+++ .github/scripts/pr_mod_file_tests.py
@@ -71,10 +71,10 @@ def _file_is_python(filename):
 
                             #Check that first non-blank
                             #line is a shabang:
-                            if line[0:2] == '#!':
+                            if line.startswith("#!"):
                                 #If so, then check that the word
                                 #"python" is also present:
-                                if line.find("python") != -1:
+                                if "python" in line:
                                     #If the word exists, then assume
                                     #it is a python file:
                                     is_python = True
