3b27365bf652c1989b603288dcc7b971cc4d9663
Kuan-Chih Wang <kuanchihw@ucar.edu>
2024-02-13 16:49:57 -0700

    Implement `dyn_readnl`
    `dyn_readnl` calls appropriate MPAS subdriver APIs to:
    1. Read MPAS namelist from supplied path.
    2. Perform early initialization of MPAS dynamical core.

--- src/dynamics/mpas/dyn_comp.F90
+++ src/dynamics/mpas/dyn_comp.F90
@@ -1,5 +1,22 @@
 module dyn_comp
+    use dyn_mpas_subdriver, only: mpas_dynamical_core
+
+    ! Modules from CAM.
+    use cam_abortutils, only: endrun
+    use cam_control_mod, only: initial_run
+    use cam_instance, only: atm_id
+    use cam_logfile, only: iulog
     use runtime_obj, only: runtime_options
+    use spmd_utils, only: mpicom
+    use time_manager, only: get_start_date, get_stop_date, get_run_duration, timemgr_get_calendar_cf
+
+    ! Modules from CESM Share.
+    use shr_file_mod, only: shr_file_getunit
+    use shr_kind_mod, only: shr_kind_cs
+    use shr_pio_mod, only: shr_pio_getiosys
+
+    ! Modules from external libraries.
+    use pio, only: iosystem_desc_t
 
     implicit none
 
@@ -18,11 +35,60 @@ module dyn_comp
     type dyn_export_t
     end type dyn_export_t
 contains
+    !> Read MPAS namelist from supplied path.
+    !> Additionally, perform early initialization of MPAS dynamical core.
+    !> (KCW, 2024-02-09)
+    !
+    ! Called by `read_namelist` in `src/control/runtime_opts.F90`.
+    subroutine dyn_readnl(namelist_path)
+        character(*), intent(in) :: namelist_path
 
-! Called by `read_namelist` in `src/control/runtime_opts.F90`.
-subroutine dyn_readnl(namelist_path)
-    character(*), intent(in) :: namelist_path
-end subroutine dyn_readnl
+        character(shr_kind_cs) :: cam_calendar
+        integer :: log_unit(2)
+        integer :: start_date_time(6), & ! YYYY, MM, DD, hh, mm, ss.
+                   stop_date_time(6),  & ! YYYY, MM, DD, hh, mm, ss.
+                   run_duration(4),    & ! DD, hh, mm, ss.
+                   sec_since_midnight    ! Second(s) since midnight.
+        type(iosystem_desc_t), pointer :: pio_iosystem => null()
+
+        ! Get free units for MPAS so it can write its own log files, e.g., `log.atmosphere.0000.{out,err}`.
+        log_unit(1) = shr_file_getunit()
+        log_unit(2) = shr_file_getunit()
+
+        call mpas_dynamical_core % init_phase1(mpicom, endrun, iulog, log_unit)
+
+        cam_calendar = timemgr_get_calendar_cf()
+
+        call get_start_date(start_date_time(1), start_date_time(2), start_date_time(3), sec_since_midnight)
+        start_date_time(4:6) = sec_to_hour_min_sec(sec_since_midnight)
+
+        call get_stop_date(stop_date_time(1), stop_date_time(2), stop_date_time(3), sec_since_midnight)
+        stop_date_time(4:6) = sec_to_hour_min_sec(sec_since_midnight)
+
+        call get_run_duration(run_duration(1), sec_since_midnight)
+        run_duration(2:4) = sec_to_hour_min_sec(sec_since_midnight)
+
+        call mpas_dynamical_core % read_namelist(namelist_path, &
+            cam_calendar, start_date_time, stop_date_time, run_duration, initial_run)
+
+        pio_iosystem => shr_pio_getiosys(atm_id)
+
+        call mpas_dynamical_core % init_phase2(pio_iosystem)
+
+        nullify(pio_iosystem)
+    contains
+        !> Convert second(s) to hour(s), minute(s), and second(s).
+        !> (KCW, 2024-02-07)
+        pure function sec_to_hour_min_sec(sec) result(hour_min_sec)
+            integer, intent(in) :: sec
+            integer :: hour_min_sec(3)
+
+            ! These are all intended to be integer divisions.
+            hour_min_sec(1) = sec / 3600
+            hour_min_sec(2) = sec / 60 - hour_min_sec(1) * 60
+            hour_min_sec(3) = sec - hour_min_sec(1) * 3600 - hour_min_sec(2) * 60
+        end function sec_to_hour_min_sec
+    end subroutine dyn_readnl
 
 ! Called by `cam_init` in `src/control/cam_comp.F90`.
 subroutine dyn_init(cam_runtime_opts, dyn_in, dyn_out)
