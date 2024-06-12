fc53040e7861681ac9f9614361cf87c36874ccfb
Jesse Nusbaumer <nusbaume@ucar.edu>
2024-06-12 15:01:44 -0600

    Add new timestep number variable and root processor check.

--- src/utils/time_manager.F90
+++ src/utils/time_manager.F90
@@ -521,7 +521,11 @@ subroutine advance_timestep()
 ! Increment the timestep number.
 
 ! Use statements
-   use string_utils, only: stringify
+   use ESMF,          only: ESMF_ClockAdvance
+   use cam_logfile,   only: iulog
+   use physics_types, only: nstep
+   use spmd_utils,    only: masterproc
+   use string_utils,  only: stringify
 
 ! Local variables
    character(len=*), parameter :: sub = 'advance_timestep'
@@ -531,11 +535,16 @@ subroutine advance_timestep()
    call ESMF_ClockAdvance( tm_clock, rc=rc )
    call chkrc(rc, sub//': error return from ESMF_ClockAdvance')
 
+! Set current timestep number for use in CCPP physics schemes:
+   nstep = get_nstep()
+
 ! Write new timestep to CAM log file.
 
-   write(iulog,*) '------------------------'
-   write(iulog,*) 'CAM-SIMA time step advanced (nstep = '//stringify([get_nstep()])//')'
-   write(iulog,*) '------------------------'
+   if (masterproc) then
+      write(iulog,*) '------------------------'
+      write(iulog,*) 'CAM-SIMA time step advanced (nstep = '//stringify([nstep])//')'
+      write(iulog,*) '------------------------'
+   end if
 
 ! Set first step flag off.
 
