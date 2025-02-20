7e02d9045b36c5ee1fd5d006f7cce54681b2c354
Jiwon Gim <jiwongim@ucar.edu>
2025-02-19 19:30:08 -0700

    update docker to build case

--- cime_config/atm_musica_config.py
+++ cime_config/atm_musica_config.py
@@ -1,9 +1,9 @@
 """
-The URLs and tags provided in this script are read by buildlib.py to build 
+The URLs and tags provided in this script are read by buildlib to build
 and install the MUSICA library, as well as to download the MUSICA configuration."
 """
 
 musica_repo_url = "https://github.com/NCAR/musica.git"
-musica_tag = "e0c3d219fc8bbfd388cd3c95a3646d2e3aa1e288" # main
+musica_tag = "802bc9041f964a7edd5ec896bec8cdc0c90de50b"
 chemistry_data_repo_url = "https://github.com/NCAR/cam-sima-chemistry-data.git"
 chemistry_data_tag = "b81cbc2e61c41ecd2d09167d6736daf1bf1be149"
