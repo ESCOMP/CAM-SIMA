2965f952b08ecdf4515abd1d9368a7ef9b55f3a1
Jiwon Gim <jiwongim@ucar.edu>
2025-03-05 20:45:38 -0700

    remove symlink

--- cime_config/atm_musica_config.py
+++ cime_config/atm_musica_config.py
@@ -1,10 +1,10 @@
 """
 The URLs and tags provided in this script are read by buildlib to build
-and install the MUSICA library, as well as to download the MUSICA configuration."
+and install the MUSICA library, as well as to download the MUSICA configuration.
 """
 
 MUSICA_CCPP_SCHEME_NAME = "musica_ccpp"
 MUSICA_REPO_URL = "https://github.com/NCAR/musica.git"
 MUSICA_TAG = "cc39bb00d2220fc81c85b22d3ceea4a39bd2bacf"
 CHEMISTRY_DATA_REPO_URL = "https://github.com/NCAR/cam-sima-chemistry-data.git"
-CHEMISTRY_DATA_TAG = "b81cbc2e61c41ecd2d09167d6736daf1bf1be149"
+CHEMISTRY_DATA_TAG = "2b58f2410ec7a565bcf80dee16ec20f6bc35d78b"
