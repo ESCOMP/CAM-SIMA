2cacf106450d624644966bbf71e6da7d76cfd069
Jiwon Gim <jiwongim@ucar.edu>
2025-03-19 17:50:18 -0600

    update cam sima data repo commit

--- cime_config/atm_musica_config.py
+++ cime_config/atm_musica_config.py
@@ -7,4 +7,4 @@ MUSICA_CCPP_SCHEME_NAME = "musica_ccpp"
 MUSICA_REPO_URL = "https://github.com/NCAR/musica.git"
 MUSICA_TAG = "cc39bb00d2220fc81c85b22d3ceea4a39bd2bacf"
 CHEMISTRY_DATA_REPO_URL = "https://github.com/NCAR/cam-sima-chemistry-data.git"
-CHEMISTRY_DATA_TAG = "b575c8232b85ab99e810274e5e1e9fce97d84bc2" # temp
+CHEMISTRY_DATA_TAG = "cb57c11dd72de2b116157123e4f8d76ffeae21af" # temp
