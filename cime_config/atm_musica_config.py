845ed02a5604749ec8c9ce6f2361e51f2d53bd63
Jiwon Gim <jiwongim@ucar.edu>
2025-03-26 17:02:11 -0600

    update the tag for musica data

--- cime_config/atm_musica_config.py
+++ cime_config/atm_musica_config.py
@@ -8,4 +8,4 @@ MUSICA_CONFIG_DIR_NAME = "musica_configurations"
 MUSICA_REPO_URL = "https://github.com/NCAR/musica.git"
 MUSICA_TAG = "cc39bb00d2220fc81c85b22d3ceea4a39bd2bacf"
 CHEMISTRY_DATA_REPO_URL = "https://github.com/NCAR/cam-sima-chemistry-data.git"
-CHEMISTRY_DATA_TAG = "71ed143c54b0d5d6e3e70f3d05d413fddcf8d59e" # temp
+CHEMISTRY_DATA_TAG = "71ed143c54b0d5d6e3e70f3d05d413fddcf8d59e"
