aae8fafb706135bdd013303b976a6b407b57c92c
Jesse Nusbaumer <nusbaume@ucar.edu>
2025-06-20 11:30:44 -0600

    Try removing "oldmethod" option.

--- test/unit/fortran/utils/cam_pio_utils.F90
+++ test/unit/fortran/utils/cam_pio_utils.F90
@@ -58,8 +58,7 @@ contains
       ! If an error code is present, make sure PIO returns an error
       ! instead of aborting:
       if (present(errcode)) then
-         call pio_seterrorhandling(file, PIO_BCAST_ERROR, &
-         oldmethod=err_handling)
+         call pio_seterrorhandling(file, PIO_BCAST_ERROR) !, oldmethod=err_handling)
       end if
 
       ierr = pio_openfile(pio_system, file, pio_iotype_netcdf, fname, mode)
@@ -81,9 +80,9 @@ contains
       ! If an error code is requested, then set
       ! the error handling back to whatever was
       ! running before this routine:
-      if (present(errcode)) then
-         call pio_seterrorhandling(file, err_handling)
-      end if
+      !if (present(errcode)) then
+         !call pio_seterrorhandling(file, err_handling)
+      !end if
 
    end subroutine cam_pio_openfile
    !===========================================================================
