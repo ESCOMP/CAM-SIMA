28a8d9a04b0367c1163bfa441600579fed7b9b09
Jesse Nusbaumer <nusbaume@ucar.edu>
2026-08-28 12:31:04 -0600

    Remove duplicate registry build cache listings found by Claude Opus 5.

--- cime_config/cam_autogen.py
+++ cime_config/cam_autogen.py
@@ -465,8 +465,11 @@ def generate_registry(data_search, build_cache, atm_root, bldroot,
             reg_files_list += reg_file_list
         # End for
 
-        # Save build details in the build cache
-        reg_file_paths = [x.file_path for x in reg_file_list if x.file_path]
+        # Save build details in the build cache,
+        # removing any duplicate metadata entries
+        # created by the registry generator:
+        reg_file_paths = list(dict.fromkeys(
+            x.file_path for x in reg_file_list if x.file_path))
         build_cache.update_registry(gen_reg_file, registry_files, dycore,
                                     reg_file_paths, ic_names, registry_constituents, vars_init_value)
     else:
